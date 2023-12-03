f0d477e1cc71adcaef15541316fd8cd215ef9af3
dlin <lindevin101@gmail.com>
2023-12-02 23:03:59 -0500

    updating app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from peewee import *
+import datetime
 
-# Initialize Flask
-# We'll use the pre-defined global '__name__' variable to tell Flask where it is. 
-# More on this below.
+db = PostgresqlData('water', user='poowoo', password='123456', host='localhost', port=5432)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Bottle(BaseModel):
+    brand = CharField()
+    source = CharField()
+    location = CharField()
+    rating = FloatField()
+
+db.connect()
+db.drop_table([Bottle])
+db.create_tables([Bottle])
+
 app = Flask(__name__)
 
-# Define our route
-# This syntax is using a Python decorator, which is essentially a succinct way to wrap a function in another function.
 @app.route('/')
 def index():
-  return "Hello, world!"
+    return "Hello, world!"
 
+Bottle(brand='Arrowhead', source='Springwater', location='San Bernadino, CA', ph=7.93).save()
+Bottle(brand='Poland Spring', source='Springwater', location='Poland Spring, ME', ph=6.4).save()
+Bottle(brand='Sparkletts', source='Artesian', location='Various', ph=6.9).save()
+Bottle(brand='Evian', source='Springwater', location='Evian, France', ph=7.2).save()
 # Run our application, by default on port 5000
-app.run()
+app.run(debug=True)
